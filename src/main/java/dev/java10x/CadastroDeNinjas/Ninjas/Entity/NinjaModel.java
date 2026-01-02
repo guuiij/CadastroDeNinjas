902cc36040e2825ac351426600092d23c966aeba
guuiij <guilherme.jadson@hotmail.com>
2026-01-02 16:49:50 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Entity/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Entity/NinjaModel.java
@@ -2,14 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Entity;
 
 import dev.java10x.CadastroDeNinjas.Missoes.Entity.MissoesModel;
 import jakarta.persistence.*;
-
-
-import java.util.List;
+import lombok.*;
 
 // Entity transforma uma classe em uma entidade do BD
 // JPA – Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -28,37 +29,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")  // Foreing Key ou chave estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
