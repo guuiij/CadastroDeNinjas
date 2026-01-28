b9a4de8df8edb95b2c3a71f12d9fd7153cc79865
guuiij <guilherme.jadson@hotmail.com>
2026-01-28 15:23:24 -0300

    chore(missoes): implementa conversão manual entre MissaoDTO e MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/mapper/MissaoMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/mapper/MissaoMapper.java
@@ -1,4 +1,30 @@
 package dev.java10x.CadastroDeNinjas.Missoes.mapper;
 
+import dev.java10x.CadastroDeNinjas.Missoes.Entity.MissoesModel;
+import dev.java10x.CadastroDeNinjas.Missoes.dto.MissaoDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissaoMapper {
+
+    public MissoesModel map(MissaoDTO missaoDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missaoDTO.getId());
+        missoesModel.setNome(missaoDTO.getNome());
+        missoesModel.setDificuldade(missaoDTO.getDificuldade());
+        missoesModel.setNinjas(missaoDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    public MissaoDTO map(MissoesModel missoesModel){
+        MissaoDTO missaoDTO = new MissaoDTO();
+        missaoDTO.setId(missoesModel.getId());
+        missaoDTO.setNome(missoesModel.getNome());
+        missaoDTO.setDificuldade(missoesModel.getDificuldade());
+        missaoDTO.setNinjas(missoesModel.getNinjas());
+
+        return missaoDTO;
+    }
+
 }
